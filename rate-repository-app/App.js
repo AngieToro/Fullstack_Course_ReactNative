845a22a009d35a34d9b2c103f8360100a21ace74
Angelica Toro <angelicatoro88@gmail.com>
2025-07-24 10:52:52 -0400

    Actualizacion

--- rate-repository-app/App.js
+++ rate-repository-app/App.js
@@ -5,15 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-<<<<<<< Updated upstream
       <Text>Hello World React Native</Text>
-=======
-<<<<<<< HEAD
-      <Text>Hello</Text>
-=======
-      <Text>Hello World React Native</Text>
->>>>>>> d1ee9ed (Actualizacion de referencias)
->>>>>>> Stashed changes
       <StatusBar style="auto" />
     </View>
   );
